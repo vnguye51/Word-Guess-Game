0ac8a01a59619b1d57b4886aafb283c6e8b90784
Vincent Nguyen <vincenttsnguyen@gmail.com>
2018-07-31 16:08:17 -0700

    Styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,28 @@
 
 var game = 
 {
-    nameList: ['Chihiro','Genkai','Hamtaro','Ichigo','Inuyasha','Killua','Kirito','Krillin','Luffy','Renji','Sasuke','Taiga Aisaka','Tohru Honda','Vash the Stampede','Yamcha.jpg','Yusuke Urameshi'].sort(),
+    nameList: ['Chihiro','Genkai','Hamtaro','Ichigo Kurosaki','Inuyasha','Killua','Kirito','Krillin','Luffy','Renji','Sasuke Uchiha','Taiga Aisaka','Tohru Honda','Vash the Stampede','Yamcha','Yusuke Urameshi'].sort(),
     imageList: ['Chihiro.jpg','Genkai.jpg','Hamtaro.png','Ichigo.png','Inuyasha.jpg','Killua.png','Kirito.png','Krillin.png','Luffy.png','Renji.png','Sasuke.jpg','Taiga.jpg','Tohru.jpg','Vash.jpg','Yamcha.jpg','Yusuke.png'].sort(),
     index: 0,
     name: '',
     nameSoFar: [],
     score: 0,
+    targetscore: 0,
     previousGuesses: [],
     guessesLeft: 10,
 
-    start: function() 
+    start: function()
+    {
+        document.getElementById('Guesses').removeAttribute("hidden")
+        document.getElementById('Score').removeAttribute("hidden")
+        document.getElementById('AgainButton').removeAttribute('hidden')
+        document.getElementById('Tries').removeAttribute("hidden")
+        document.getElementById('AnimePic').removeAttribute("hidden")
+        document.getElementById("StartButton").setAttribute("hidden",true)
+        this.playAgain()
+    },
+
+    playAgain: function() 
     {
         this.previousGuesses = []
         this.nameSoFar = []
@@ -19,27 +31,23 @@ var game =
         this.index = (Math.floor(Math.random()*(this.nameList.length)));
         this.name = (this.nameList[this.index]).toLowerCase();
         document.getElementById('AnimePic').src = 'assets/images/' + this.imageList[this.index]
-        console.log('1');
 
-        console.log(this.name);
-        console.log(this.name.length);
+        this.targetscore = this.name.length
         for (var i = 0; i < this.name.length; i++)
         {
             this.nameSoFar.push('_');
+            if (this.name[i] == ' ')
+            {
+                this.targetscore -= 1
+                this.nameSoFar[i] = ' '
+            }
         }
-        console.log('2')
-
-        
 
         // Remove index from possible choices
         this.nameList.splice(this.index,1)
-        this.imageList.splice(this.index,1)
-
-       
-       
+        this.imageList.splice(this.index,1)   
 
         document.getElementById("Win").setAttribute("hidden",true)
-        document.getElementById("StartButton").setAttribute("hidden",true)
         document.getElementById("Lose").setAttribute("hidden",true);
         document.getElementById("Guesses").innerHTML = "Your guesses: " + this.previousGuesses.join('')
         document.getElementById("Tries").innerHTML = "Tries Left: " + this.guessesLeft
@@ -56,17 +64,26 @@ var game =
             if (this.name[i] === c)
             {
                 count += 1;
-                this.nameSoFar[i] = c
+                if ((i == 0) || ( this.name[i-1] == ' ')) //If the character is the first one or is after a space capitalize it
+                {
+                    this.nameSoFar[i] = c.toUpperCase()
+                }
+                else //Otherwise just output it normally
+                {
+                    this.nameSoFar[i] = c;
+                }
+                
             }
         }
         return count;
     },
 
 
+
     //Is called whenever a key is pressed. Contains win/loss logic.
     guess: function(event) 
     {
-        var c = event.key;
+        var c = event.key.toLowerCase();
         if (!(this.previousGuesses.includes(c)))
         {
             this.previousGuesses.push(c);
@@ -82,10 +99,10 @@ var game =
                 this.score += count;
                 document.getElementById("Score").innerHTML = this.nameSoFar.join('')
                 //If all characters have been matched: You win!
-                if (this.score === this.name.length)
+                if (this.score === this.targetscore)
                 {
                     document.getElementById("Win").removeAttribute("hidden");
-                    document.getElementById("StartButton").removeAttribute("hidden")
+                    document.getElementById("AgainButton").removeAttribute("hidden")
                 }
             }
 
@@ -99,7 +116,7 @@ var game =
                 if (this.guessesLeft === 0)
                 {
                     document.getElementById("Lose").removeAttribute("hidden");
-                    document.getElementById("StartButton").removeAttribute("hidden")
+                    document.getElementById("AgainButton").removeAttribute("hidden")
 
                 }
             }
@@ -108,18 +125,12 @@ var game =
 }
 
 
-//Initialize the values on page load
-game.start()
-
-
 //Call the guess function on key input
-document.onkeyup = function(event) {
-    if ((event.key.charCodeAt(0) >= 97) && (event.key.charCodeAt(0) <= 122) || (event.key.charCodeAt(0) === 32)) //Only allow a-z characters
+document.onkeypress = function(event) {
+    if ((event.key.charCodeAt(0) >= 97) && (event.key.charCodeAt(0) <= 122)) //Only allow a-z characters
     {
         game.guess(event)
     }
     
 
 }
-
-
