72dc9cf42a8662f06917496d217549e55c6b0e4a
Vincent Nguyen <vincenttsnguyen@gmail.com>
2018-07-31 22:38:48 -0700

    Added score and media queries

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,50 @@
 
 var game = 
-{
-    nameList: ['Chihiro','Genkai','Hamtaro','Ichigo Kurosaki','Inuyasha','Killua','Kirito','Krillin','Luffy','Renji','Sasuke Uchiha','Taiga Aisaka','Tohru Honda','Vash the Stampede','Yamcha','Yusuke Urameshi'].sort(),
-    imageList: ['Chihiro.jpg','Genkai.jpg','Hamtaro.png','Ichigo.png','Inuyasha.jpg','Killua.png','Kirito.png','Krillin.png','Luffy.png','Renji.png','Sasuke.jpg','Taiga.jpg','Tohru.jpg','Vash.jpg','Yamcha.jpg','Yusuke.png'].sort(),
+{   //Maybe retrieve names and images from databases later on
+    nameList: ['Rurouni Kenshin','Gary Oak','Joey Wheeler','Alphonse Elric', 'Chihiro','Genkai','Hamtaro','Ichigo Kurosaki','Inuyasha','Killua','Kirito','Krillin','Luffy','Renji','Sasuke Uchiha','Taiga Aisaka','Tohru Honda','Vash the Stampede','Yusuke Urameshi'],
+    imageList: ['Kenshin.png','Gary.jpg','Joey.jpg', 'Alphonse.jpg','Chihiro.jpg','Genkai.jpg','Hamtaro.png','Ichigo.png','Inuyasha.jpg','Killua.png','Kirito.png','Krillin.png','Luffy.png','Renji.png','Sasuke.jpg','Taiga.jpg','Tohru.jpg','Vash.jpg','Yusuke.png'],
     index: 0,
     name: '',
     nameSoFar: [],
+    subScore: 0,
     score: 0,
+    possibleScore: 0,
     targetscore: 0,
     previousGuesses: [],
     guessesLeft: 10,
+    gameIntermission: false,
+
 
     start: function()
     {
         document.getElementById('Guesses').removeAttribute("hidden")
-        document.getElementById('Score').removeAttribute("hidden")
-        document.getElementById('AgainButton').removeAttribute('hidden')
+        document.getElementById('SubScore').removeAttribute("hidden")
         document.getElementById('Tries').removeAttribute("hidden")
         document.getElementById('AnimePic').removeAttribute("hidden")
-        document.getElementById("StartButton").setAttribute("hidden",true)
+        document.getElementById('Characters').removeAttribute("hidden")
+        document.getElementById('Score').removeAttribute('hidden')
+
+        document.getElementById("EasyButton").setAttribute("hidden",true)
+        document.getElementById("MediumButton").setAttribute("hidden",true)
+        document.getElementById("HardButton").setAttribute("hidden",true)
+
         this.playAgain()
     },
 
     playAgain: function() 
     {
+        //Reinitialize values
+        this.possibleScore += 1
+        this.gameIntermission = false
         this.previousGuesses = []
         this.nameSoFar = []
-        this.score = 0;
+        this.subScore = 0;
         this.guessesLeft = 10;
         this.index = (Math.floor(Math.random()*(this.nameList.length)));
         this.name = (this.nameList[this.index]).toLowerCase();
+
         document.getElementById('AnimePic').src = 'assets/images/' + this.imageList[this.index]
+        document.getElementById("Characters").textContent = 'Characters Left: ' + this.nameList.length
 
         this.targetscore = this.name.length
         for (var i = 0; i < this.name.length; i++)
@@ -51,7 +65,9 @@ var game =
         document.getElementById("Lose").setAttribute("hidden",true);
         document.getElementById("Guesses").innerHTML = "Your guesses: " + this.previousGuesses.join('')
         document.getElementById("Tries").innerHTML = "Tries Left: " + this.guessesLeft
-        document.getElementById("Score").innerHTML = this.nameSoFar.join('')
+        document.getElementById("SubScore").innerHTML = this.nameSoFar.join('')
+        document.getElementById("AgainButton").setAttribute("hidden",true)
+
     },
 
 
@@ -81,9 +97,8 @@ var game =
 
 
     //Is called whenever a key is pressed. Contains win/loss logic.
-    guess: function(event) 
+    guess: function(c) 
     {
-        var c = event.key.toLowerCase();
         if (!(this.previousGuesses.includes(c)))
         {
             this.previousGuesses.push(c);
@@ -96,13 +111,17 @@ var game =
             //If there is an instance of the guess, increment the score(# of characters matched)
             if (count != 0)
             {
-                this.score += count;
-                document.getElementById("Score").innerHTML = this.nameSoFar.join('')
+                this.subScore += count;
+                document.getElementById("SubScore").innerHTML = this.nameSoFar.join('')
                 //If all characters have been matched: You win!
-                if (this.score === this.targetscore)
+                if (this.subScore === this.targetscore)
                 {
+                    this.score += 1
                     document.getElementById("Win").removeAttribute("hidden");
                     document.getElementById("AgainButton").removeAttribute("hidden")
+                    document.getElementById("Score").textContent = 'Score: ' + this.score + '|' + this.possibleScore
+                    game.gameIntermission = true
+
                 }
             }
 
@@ -117,6 +136,8 @@ var game =
                 {
                     document.getElementById("Lose").removeAttribute("hidden");
                     document.getElementById("AgainButton").removeAttribute("hidden")
+                    document.getElementById("Score").textContent = 'Score: ' + this.score + '|' + this.possibleScore
+                    game.gameIntermission = true
 
                 }
             }
@@ -127,10 +148,13 @@ var game =
 
 //Call the guess function on key input
 document.onkeypress = function(event) {
-    if ((event.key.charCodeAt(0) >= 97) && (event.key.charCodeAt(0) <= 122)) //Only allow a-z characters
+    if (game.gameIntermission == false)
     {
-        game.guess(event)
-    }
-    
+        var c = event.key.toLowerCase()
+        if ((c.charCodeAt(0) >= 97) && (c.charCodeAt(0) <= 122)) //Only allow a-z characters
+        {
+            game.guess(c)
+        }
+    }   
 
 }
